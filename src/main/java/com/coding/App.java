fb69c31fc6b2abb04a0169df20303db7f22466d2
donnabauerc <donnabauer.christian.01@gmail.com>
2021-08-14 17:07:19 +0200

    small improvements

--- src/main/java/com/coding/App.java
+++ src/main/java/com/coding/App.java
@@ -10,33 +10,35 @@ public class App
     public static void main( String[] args )
     {
         char choice;
+        String input = "";
         Scanner sc = new Scanner(System.in);
-        System.out.println( "Welcome!" );
+        System.out.println("++++++++ Welcome! ++++++++");
         System.out.println("To exit the program press e");
 
         do {
-            String input = "";
+            System.out.println();
             System.out.print("If you want to ask a question please press q. To add a new question press a: ");
             choice = sc.next().charAt(0);
 
             sc = new Scanner(System.in);
+
             switch (choice) {
-                case 'q':
+                case 'q': // User asks Question
                     System.out.print("Ask your question: ");
                     input = sc.nextLine();
+
                     try {
                         stored_questions.get(input).forEach(a -> System.out.println(a));
                     } catch (Exception e) {
                         System.out.println("The answer to life, universe and everything is 42");
                     }
-
                     break;
-                case 'a':
+                case 'a': // User adds Question and Answers
                     System.out.print("Add your question (<question>? \"<answer1>\" \"<answer2>\" \"<answerX>\"): ");
                     input = sc.nextLine();
                     addQuestion(input);
                     break;
-                case 'e':
+                case 'e': // User stops program
                     break;
                 default:
                     System.out.println("Sorry, unknown input!");
@@ -46,18 +48,22 @@ public class App
         System.out.println("Have a nice day!");
     }
 
-    public static void addQuestion(String input) { //TODO: Check Question Format
-        LinkedList<String> answers = new LinkedList<String>();
-        String question = input.split("\\?")[0];
-        question = input.substring(0, input.indexOf("?") + 1);
-        String[] answer_part = input.split("\\?")[1].split("\"");
+    public static void addQuestion(String input) {
 
-        for (int i = 0; i < answer_part.length; i++) {
-            if (i%2 != 0) {
-                answers.push(answer_part[i]);
+        if(input.contains("?") && input.indexOf('"', input.indexOf('"') + 1) > -1) { // check if input contains at least one question & answer
+            LinkedList<String> answers = new LinkedList<String>();
+            String question = input.substring(0, input.indexOf("?") + 1); // get question part 
+            String[] answer_part = input.split("\\?")[1].split("\""); // get answer part
+
+            for (int i = 0; i < answer_part.length; i++) { // cut answer part into seperate answers
+                if (i%2 != 0) {
+                    answers.push(answer_part[i]);
+                }
             }
-        }
 
-        stored_questions.put(question, answers);
+            stored_questions.put(question, answers); // store question & answers in map
+        } else {
+            System.out.println("Sorry, unknown input format!");
+        }
     }
 }
